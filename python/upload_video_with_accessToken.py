75ef4b573ef1da0815cbc8b3534feac628d22aa4
sug1no <3293067+sug1no@users.noreply.github.com>
2020-11-04 11:28:01 +0900

    [WIP]: remove token refresh and auth depend codes.

--- python/upload_video_with_accessToken.py
+++ python/upload_video_with_accessToken.py
@@ -13,7 +13,6 @@ import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
-from google_auth_oauthlib.flow import InstalledAppFlow
 
 from logging import getLogger
 logger = getLogger(__name__)
@@ -35,22 +34,9 @@ RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, http.client.NotConnecte
 # codes is raised.
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
-# the {{ Google Cloud Console }} at
-# {{ https://cloud.google.com/console }}.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-# specify clientid and secret to verify and use to inboke youtube API (ex.. for webapp).
-CLIENT_SECRETS_FILE = 'client_secret_webapp.json'
-
 # This OAuth 2.0 access scope allows an application to upload files to the
 # authenticated user's YouTube channel, but doesn't allow other types of access.
-SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+# SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
@@ -59,18 +45,10 @@ VALID_PRIVACY_STATUSES = ('public', 'private', 'unlisted')
 
 # Authorize the request and store authorization credentials.
 def get_authenticated_service():
-  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  accessToken = input('Enter the authorized access_token: ')
 
-  clientId = flow.client_config['client_id']
-  clienSecret = flow.client_config['client_secret']
-  accessToken = input('Enter the authorized access_token for client_id=%s: ' % clientId)
- 
   try:
-    credentials = google.oauth2.credentials.Credentials(token=accessToken,
-      client_id=clientId,
-      client_secret=clienSecret,
-      token_uri=flow.client_config['token_uri']
-      );
+    credentials = google.oauth2.credentials.Credentials(token=accessToken);
 
   except Exception as e:
     logger.error("failed to create Credentials: [{}] [{}]".format(id_token, e))
